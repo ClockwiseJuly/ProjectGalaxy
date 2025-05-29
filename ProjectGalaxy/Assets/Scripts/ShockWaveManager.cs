371a9ba7e7c87807f9f8fadeb80b1be62eb3bea1
Autumnind-a <2622145833@qq.com>
2025-05-29 14:13:53 +0800

    ShockWave

---
+++ ProjectGalaxy/Assets/Scripts/ShockWaveManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockWaveManager : MonoBehaviour
+{
+    [SerializeField] private float shockWaveTime = 0.75f;
+    private Coroutine shockWaveCoroutine;
+    [SerializeField] private Material shockWaveMaterial;
+    
+    private static int waveDistanceFromCenter = Shader.PropertyToID("_WaveDistanceFormCenter");
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CallShockWave();
+        }
+    }
+
+    public void CallShockWave()
+    {
+        shockWaveCoroutine = StartCoroutine(ShockWaveAction(-0.1f, 1f));
+    }
+
+    private IEnumerator ShockWaveAction(float startPos , float endPos)
+    {
+        shockWaveMaterial.SetFloat(waveDistanceFromCenter, startPos);
+
+        float lerpedAmount = 0f;
+        float elapsedTime = 0f;
+        while (elapsedTime < shockWaveTime)
+        {
+            elapsedTime += Time.deltaTime;
+            lerpedAmount = Mathf.Lerp(startPos, endPos, elapsedTime / shockWaveTime);
+            shockWaveMaterial.SetFloat(waveDistanceFromCenter, lerpedAmount);
+
+            yield return null;
+        }
+
+    }
+}
--- ProjectGalaxy/Assets/Scripts/test.cs
+++ ProjectGalaxy/Assets/Scripts/test.cs
@@ -1,18 +1,126 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
-public class test : MonoBehaviour
+
+public class Test : ScriptableRendererFeature
 {
-    // Start is called before the first frame update
-    void Start()
+    // [SerializeField]
+    // private Material m;
+    // private Vector2 distorCenter;
+    // [Range(0,1)][SerializeField]
+    // private float distorValue;
+    //
+    // private void Start()
+    // {
+    //     distorCenter = new Vector2(0.5f , 0.5f);
+    //     if(m==null)
+    //         m = new Material(Shader.Find("Autumnind/TraverseShader"));
+    // }
+    //
+    // private void Update()
+    // {
+    //     
+    // }
+    //
+    // private void OnRenderImage(RenderTexture a , RenderTexture b)
+    // {
+    //     if (m != null)
+    //     {
+    //         m.SetFloat("_DistorValue", distorValue);
+    //         m.SetVector("_DistorCenter", distorCenter);
+    //         
+    //         Graphics.Blit(a,b,m);
+    //     }
+    //     else
+    //     {
+    //         Graphics.Blit(a,b);
+    //     }
+    // }
+    
+    [System.Serializable]
+    public class Settings
     {
-        Debug.Log("Hello World!");
+        public Material material;
+        [Range(0, 0.5f)] public float distortionValue = 0.1f;
+        public Vector2 distortionCenter = new Vector2(0.5f, 0.5f);
+        public RenderPassEvent renderEvent = RenderPassEvent.AfterRenderingPostProcessing;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Settings settings = new Settings();
+    private TraverseRenderPass renderPass;
+
+    public override void Create()
     {
+        if (settings.material == null)
+        {
+            Debug.LogWarning("Traverse效果缺少材质");
+            return;
+        }
         
+        renderPass = new TraverseRenderPass(settings.material)
+        {
+            renderPassEvent = settings.renderEvent
+        };
     }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (settings.material == null) return;
+        
+        renderPass.SetDistortionParams(settings.distortionValue, settings.distortionCenter);
+        renderPass.Setup(renderer.cameraColorTarget);
+        renderer.EnqueuePass(renderPass);
+    }
+
+    class TraverseRenderPass : ScriptableRenderPass
+    {
+        private Material material;
+        private RenderTargetIdentifier source;
+        private RenderTargetHandle tempTexture;
+        private float distortionValue;
+        private Vector2 distortionCenter;
+
+        public TraverseRenderPass(Material material)
+        {
+            this.material = material;
+            tempTexture.Init("_TempTraverseTexture");
+        }
+
+        public void SetDistortionParams(float value, Vector2 center)
+        {
+            distortionValue = value;
+            distortionCenter = center;
+        }
+
+        public void Setup(RenderTargetIdentifier source)
+        {
+            this.source = source;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            if (material == null) return;
+
+            CommandBuffer cmd = CommandBufferPool.Get("TraverseEffect");
+            
+            material.SetFloat("_DistorValue", distortionValue);
+            material.SetVector("_DistorCenter", new Vector4(distortionCenter.x, distortionCenter.y, 0, 0));
+            
+            RenderTextureDescriptor descriptor = renderingData.cameraData.cameraTargetDescriptor;
+            descriptor.depthBufferBits = 0;
+            
+            cmd.GetTemporaryRT(tempTexture.id, descriptor, FilterMode.Bilinear);
+            Blit(cmd, source, tempTexture.Identifier(), material);
+            Blit(cmd, tempTexture.Identifier(), source);
+            cmd.ReleaseTemporaryRT(tempTexture.id);
+            
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+    
 }
